feda1c2deded41e3a52943a226c30d342f9d4070
Faisal Javed <faisal@faisals-air.gateway.sasktel.net>
2022-11-09 16:39:00 -0600

    added fill array with  random values function

--- ShakerSort.cpp
+++ ShakerSort.cpp
@@ -1,5 +1,29 @@
-//
-// Created by Faisal Javed on 09/11/22.
-//
 
+#include <iostream>
+#include <cstdlib>
+#include <chrono>
+#include <iomanip>
 #include "ShakerSort.h"
+
+void ShakerSort::fillArrayWithRandomValues() {
+
+    srand(time(0));
+
+    array = new int[SIZE_OF_ARRAY];
+
+    for (int i = 0U; i < SIZE_OF_ARRAY; i++) {
+        array[i] = rand() % SIZE_OF_ARRAY + 1;
+    }
+}
+
+void ShakerSort::shakerSortAscending() {
+
+}
+
+void ShakerSort::shakerSortDescending() {
+
+}
+
+int ShakerSort::getArraySizeInput() {
+    return 0;
+}
